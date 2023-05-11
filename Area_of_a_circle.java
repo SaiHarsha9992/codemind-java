9ab32af3260169645be818fcf24b4d83b78162cd
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-05-11 21:48:32 +0530

    import java.util.*; public class Sample {     public static void main(String args[])     {         Scanner s = new Scanner (System.in);         int n = s.nextInt();         double l=3.14*n*n;         System.out.printf("%.2f",l);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner (System.in);
+        int n = s.nextInt();
+        double l=3.14*n*n;
+        System.out.printf("%.2f",l);
+    }
+}
