f89c246fe36e64241a9b0236174b9d6ab7e4e335
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-05-11 22:02:20 +0530

    import java.util.*; public class Sample {     public static void main(String args[])     {         Scanner s = new Scanner (System.in);         String sc = s.nextLine();         System.out.println(sc);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner (System.in);
+        String sc = s.nextLine();
+        System.out.println(sc);
+    }
+}
