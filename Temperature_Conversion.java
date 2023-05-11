c881f1a7b7522be41b41cf0cc00119fca4977db2
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-05-11 21:43:47 +0530

    import java.util.*; public class Sample {     public static void main(String args[])     {         Scanner s = new Scanner (System.in);         int f = s.nextInt();         double c=32+(f*1.8);         System.out.printf("%.2f",c);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner (System.in);
+        int f = s.nextInt();
+        double c=32+(f*1.8);
+        System.out.printf("%.2f",c);
+    }
+}
